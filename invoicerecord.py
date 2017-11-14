748aa2e3ed697d244beed842062b8ac3ff783e05
briancho94 <haejuncho@macbookpro.wireless.mountsinai.org>
2017-11-14 15:06:04 -0500

    Initial commit

---
+++ invoicerecord.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+'''
+want to import this function 
+
+eg. import 'invoicerecord.py'
+
+'''
+
+import datetime, defaultdict
+
+class MedicationRecord():
+
+	def __init__(self,data):
+		self.id = int()
+		self.name = str()
+		self.common_name = str() #Acetaminophen = Tylenol
+		self.price = defaultdict({datetime.date():float()}) 
+								#includes history
+		self.dosage = str()
+		self.admin = str()
+		self.category = str() #class of drug
+		self.prescribeable = bool()
+		self.aliases = list([]) #different names
