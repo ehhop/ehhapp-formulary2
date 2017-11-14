60d1372aecf9cd97709105119947ae099a3be9e5
briancho94 <haejuncho@macbookpro.wireless.mountsinai.org>
2017-11-14 15:24:34 -0500

    added example usage of class

---
+++ example.py
@@ -0,0 +1,13 @@
+from 'invoicerecord.py' import MedicationRecord
+
+meds = []
+new_medication = MedicationRecord()
+
+
+new_medication.price = 0.05
+new_medication.name = "atorvastatin"
+....
+...
+
+
+meds.append(new_medication)
