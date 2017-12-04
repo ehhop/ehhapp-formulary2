562fd10178fcf1946d7b42a6fc9346a71a208442
Ryan Neff <raneff@gmail.com>
2017-12-03 21:53:47 -0500

    tweak invoicerecord.py to have unique init objects

--- invoicerecord.py
+++ invoicerecord.py
@@ -11,23 +11,23 @@ import datetime, collections
 
 class MedicationRecord(object):
 	
-	id = int() #pricetable id - the item ID from the invoice
-	name = str()
-	common_name = str() #Acetaminophen = Tylenol
-	transactions = list([]) #[(class transaction(date,price,qty))]
-	dosage = str()
-	admin = str()
-	category = str() #class of drug
-	prescribable = bool()
-	aliases = list([]) #different names
-	
 	class transaction():
-		date = datetime.time()
-		price = float()
-		qty = float()
+		def __init__(self, date=None, price=None, qty=None):
+			self.date = date if date else datetime.time()
+			self.price = price if price else float()
+			self.qty = qty if qty else float()
 
-	def __init__(self, **kwargs):
-		self.__dict__.update(kwargs)
+	def __init__(self, id=None, name=None, common_name=None,transactions=None,
+		dosage=None, admin=None, category=None, prescribable=None, aliases=None):
+		self.id = id if id else int() #pricetable id - the item ID from the invoice
+		self.name = name if name else str()
+		self.common_name = common_name if common_name else str() #Acetaminophen = Tylenol
+		self.transactions = transactions if transactions else list([]) #[(class transaction(date,price,qty))]
+		self.dosage = dosage if dosage else str()
+		self.admin = admin if admin else str()
+		self.category = category if category else str() #class of drug
+		self.prescribable = prescribable if prescribable else bool()
+		self.aliases = aliases if aliases else list([]) #different names
 
 	def __repr__(self):
-		return "<MedicationRecord, id=%d, common_name=%s>" % (self.id, self.common_name)
+		return "<MedicationRecord, id=%d, name=%s, common_name=%s>" % (self.id, self.name, self.common_name)
