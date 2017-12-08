86f2a3ea457947d2466f54b6c6ad591b68585a9a
dchary <daniel.charytonowicz@icahn.mssm.edu>
2017-12-08 01:09:53 -0500

    Update to ConsolidateRecord
    Outputs csv as a list of MedicationRecord objects properly formatted.

--- invoicerecord.py
+++ invoicerecord.py
@@ -30,8 +30,24 @@ class MedicationRecord(object):
 			self.price = price
 			self.qty = qty
 
-	def __init__(self, **kwargs):
-		self.__dict__.update(kwargs)
+	class transaction():
+		def __init__(self, date=None, price=None, qty=None, source=None):
+			self.date = date if date else datetime.time()
+			self.price = price if price else float()
+			self.qty = qty if qty else float()
+			self.source = source if source else str()
+
+	def __init__(self, pricetable_id=None, name=None, common_name=None,transactions=None,
+		dosage=None, admin=None, category=None, prescribable=None, aliases=None):
+		self.pricetable_id = pricetable_id if pricetable_id else int() #pricetable id - the item ID from the invoice
+		self.name = name if name else str()
+		self.common_name = common_name if common_name else str() #Acetaminophen = Tylenol
+		self.transactions = transactions if transactions else list([]) #[(class transaction(date,price,qty))]
+		self.dosage = dosage if dosage else str()
+		self.admin = admin if admin else str()
+		self.category = category if category else str() #class of drug
+		self.prescribable = prescribable if prescribable else bool()
+		self.aliases = aliases if aliases else list([]) #different names
 
 	def __hash__(self):
 		return hash(self.pricetable_id)
