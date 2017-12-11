8a5cc71fd55aaa7ecdb5ef58ab261901985bf4f7
nosarcasm <raneff@gmail.com>
2017-12-11 18:07:07 -0500

    fixed db reset

--- reset_db.py
+++ reset_db.py
@@ -6,8 +6,7 @@ filename = "formulary2.db"
 
 try:
     os.remove(filename)
-except OSError as e: # this would be "except OSError, e:" before Python 2.6
-	if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
-		raise # re-raise exception if a different error occurred
+except OSError:
+	pass
 
 database.db.create_all()
