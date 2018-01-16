e3ccf41fa87450ab8837fb1a21e14a6db37b3ec6
Ryan Neff <raneff@gmail.com>
2018-01-15 22:27:04 -0500

    Added medications index and simple view function

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ from oauth2client import client as gauthclient
 from oauth2client import crypt
 import pytz, os, shutil, random, string, sys
 from datetime import datetime, timedelta
+import database
 
 @app.route("/", methods=['GET'])
 @app.route("/index.html", methods=['GET'])
@@ -20,3 +21,23 @@ def send_js(path):
 def add_invoice():
   #todo
   return "#TODO"
+
+@app.route("/medications")
+def view_all_medications():
+	#this is an array of type MedicationRecord objects
+	medications = database.PersistentMedication.query.\
+		order_by(database.PersistentMedication.name.asc()).\
+		all()
+	medications = [i.to_class() for i in medications]
+	return render_template("medications.html", 
+							medications=medications)
+
+@app.route("/medications/<int:pricetable_id>")
+def view_medication(pricetable_id):
+	#this is an array of type MedicationRecord objects
+	medication = database.PersistentMedication.query.\
+		filter_by(pricetable_id=pricetable_id).\
+		first_or_404()
+	medications = [medication.to_class()]
+	return render_template("medications.html", 
+							medications=medications)
