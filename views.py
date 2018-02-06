c9cdb35b5bd10a939f35c12bf5c32f51740014a1
Brian Cho <brian941204@gmail.com>
2018-02-05 21:08:24 -0500

    Pulled changes then committed

--- views.py
+++ views.py
@@ -34,42 +34,156 @@ def add_invoice():
 @app.route("/medications")
 def view_all_medications():
 	#this is an array of type MedicationRecord objects
+	year = request.values.get("year","0")
 	medications = database.PersistentMedication.query.\
 		order_by(database.PersistentMedication.name.asc()).\
 		all()
 	medications = [i.to_class() for i in medications]
-	return render_template("medications.html", 						medications=medications)
+	if year!="0":
+		medout = []
+		for m in medications:
+			m.transactions = [t for t in m.transactions if t.date.year==int(year)]
+			if len(m.transactions)!=0:
+				medout.append(m)
+		medications = medout
+	return render_template("medications.html", year=year,medications=medications)
+
+import seaborn as sb
 
 @app.route("/medications/<int:pricetable_id>")
 def view_medication(pricetable_id):
 	#this is an array of type MedicationRecord objects
+		year = request.values.get("year","2017")
+		if year=="0":
+			year="2017"
 		medication = database.PersistentMedication.query.\
-		filter_by(pricetable_id=pricetable_id).\
-		first_or_404()
+			filter_by(pricetable_id=pricetable_id).\
+			first_or_404()
 		med = medication.to_class()
-		medications = [medication.to_class()]
+		med.transactions = [t for t in med.transactions if t.date.year==int(year)]
+		medications = [med]
 		fig, (ax1,ax2) = plt.subplots(2)
+		idx = pd.date_range(year+'-01-01 00:00:00', freq='W', periods=52)
+		plt.style.use('ggplot')
+		scale = .4
+		rcParams['figure.figsize'] = (20*scale,8*scale)
+		rcParams['figure.dpi'] = 150
+		rcParams["legend.labelspacing"]=0
+		rcParams["legend.columnspacing"] = 0
+		rcParams["legend.shadow"] = False
+		rcParams["legend.frameon"] = False
+		rcParams["legend.borderpad"] = 0
+		
+		df = pd.DataFrame([{"date":t.date,"price":t.price,"qty":t.qty} for t in med.transactions if t.date.year==int(year)])
+		if len(df)==0:
+			flash("No data for that year.")
+			return redirect(url_for("view_all_medications"))
+		df.set_index("date",inplace=True)
 		if med.transactions[0].price < 1:
-			df = pd.DataFrame([{"date":t.date,"price":t.price*100,"qty":t.qty} for t in med.transactions])
-			df.plot(x="date",y="price",marker='o',ax=ax1)
-			ax1.set_title("Price history")
-			ax1.set_ylabel("Price per 100 ($)")
-			ax1.set_xlabel("Date")
+			prices = df[["price"]].resample('W').mean().loc[idx,].fillna(method='ffill').fillna(method='bfill')*100
 		else:
-			df = pd.DataFrame([{"date":t.date,"price":t.price,"qty":t.qty} for t in med.transactions])
-			df.plot(x="date",y="price",marker='o',ax=ax1)
-			ax1.set_title("Price history")
+			prices = df[["price"]].resample('W').mean().loc[idx,].fillna(method='ffill').fillna(method='bfill')
+		prices.plot(ax=ax1)
+		ax1.set_title("Price history")
+		if med.transactions[0].price < 1:
+			ax1.set_ylabel("Price per 100 ($)")
+		else:
 			ax1.set_ylabel("Price ($)")
-			ax1.set_xlabel("Date")
-		df.groupby([df["date"].dt.year, df["date"].dt.month,df["date"].dt.day])["qty"].sum().plot(kind="bar",ax=ax2)
+		ax1.set_xlabel("Date")
+		volume = df[["qty"]].resample('W').sum().loc[idx,].fillna(0)
+		volume.plot(kind="bar",ax=ax2)
 		ax2.set_title("Medication volume")
 		ax2.set_ylabel("Doses given")
 		ax2.set_xlabel("Date")
+		for tick in ax2.get_xticklabels():
+			tick.set_rotation(45)
 		fig.subplots_adjust(hspace=1.5)
 		html_figure = mpld3.fig_to_html(fig)
 		return render_template("medications_view.html", 
-				medications=medications,
-		                        html_figure=html_figure)
+				medications=medications,year=year,
+		        html_figure=html_figure)
+
+from matplotlib import rcParams
+import numpy as np
+
+@app.route("/history/")
+def view_medication_history(year=None,search_term = None):
+	search_term = request.values.get("search_term",None)
+	year = request.values.get("year","2017")
+	plt.style.use('ggplot')
+	scale = .4
+	rcParams['figure.figsize'] = (20*scale,8*scale)
+	rcParams['figure.dpi'] = 150
+	rcParams["legend.labelspacing"]=0
+	rcParams["legend.columnspacing"] = 0
+	rcParams["legend.shadow"] = False
+	rcParams["legend.frameon"] = False
+	rcParams["legend.borderpad"] = 0
+	#rcParams['font.size'] = 12
+	#rcParams['font.family'] = 'DejaVu Sans'
+	#plt.style.use('seaborn-bright')
+
+	if search_term:
+			meds = []
+			if ";" in search_term:
+				search_terms = search_term.split(";")
+				search_terms = [i.strip().upper() for i in search_terms]
+				for m in search_terms:
+					med_result = database.PersistentMedication.query.filter(database.PersistentMedication.name.contains(m)).all()
+					med_result = [i.to_class() for i in med_result]
+					meds.extend(med_result)
+			else:
+				med_to_chart = search_term.strip().upper()
+				meds = database.PersistentMedication.query.filter(database.PersistentMedication.name.contains(med_to_chart)).all()
+				meds = [i.to_class() for i in meds]
+			
+			idx = pd.date_range(year+'-01-01 00:00:00', freq='MS', periods=12)
+			price_df = pd.DataFrame(index=idx)
+			#print(idx)
+			medout = []
+			for med in meds:
+				med.transactions = [t for t in med.transactions if (t.date.year==int(year))&(t.qty>0)]
+				if med.transactions != []:
+					medout.append(med)
+				df = pd.DataFrame([{"date":t.date,"price":t.price,"qty":t.qty} for t in med.transactions])	
+				if len(df)>0:
+					df.set_index("date",inplace=True)
+					prices = df[["price"]].resample('MS').mean().loc[idx,].fillna(method='ffill').fillna(method='bfill')
+					price_df[med.name[0:20]] = prices
+			if len(price_df)==0:
+				meds=[]
+				html_figure=""
+				flash("No data in db for that search, please try again.")
+				return render_template("medications_view_history.html", 
+						medications=meds,year=year,
+						html_figure=html_figure)
+			meds = medout
+			prices = price_df.copy(deep=True)
+
+			fig, (ax1,ax2) = plt.subplots(1,2)
+			colors = plt.cm.Paired(np.linspace(0, 1, len(price_df)))
+
+			for col in range(0,len(prices.columns)):
+				for row in range(1,len(prices.index)):
+					prices.iloc[row][col] = ((prices.iloc[row][col] - prices.iloc[0][col]) / prices.iloc[0][col]) * 100
+				prices.iloc[0][col] = 0
+			prices.plot(marker="o",title="Percent change from Jan "+year,colors=colors,ax=ax1)
+			ax1.set_ylabel("% change")
+			ax1.legend(fontsize = 10,shadow=False,framealpha=0,loc="best")
+
+			price_df_100 = price_df.copy(deep=True)
+			price_df_100.plot(marker="o",title="Monthly price for "+year,colors=colors,ax=ax2)
+			ax2.set_ylabel("Price per dose ($)")
+			ax2.legend_.remove()
+
+			html_figure = mpld3.fig_to_html(fig)
+	else:
+		meds=[]
+		html_figure=""
+
+	return render_template("medications_view_history.html", 
+			medications=meds,year=year,
+	        html_figure=html_figure)
 
 @app.route("/export")
 def displayDownloadButton():
@@ -101,22 +215,19 @@ def downloadFile():
 
 @app.route("/import", methods=["GET","POST"])
 def upload_invoice():
-	if request.method == 'POST':
-		invoice_file_data = request.files['invoice_file']
-		if invoice_file_data.filename == '':
-			flash('Error: No selected file.')
-			return render_template("import.html")
-		if invoice_file_data:
-			invoice_filename = "invoice-upload-"+str(int(time.time()))+".xlsx"
-			invoice_file_data.save(import_dirname+invoice_filename)
-			msg,status = consolidateRecord.main(import_dirname+invoice_filename)
-			if status:
-				flash('Invoice added to db.')
-				return redirect(url_for("view_all_medications"))
+	if request.method == 'POST' and "invoice_file" in request.files:
+		for invoice_file_data in request.files.getlist('invoice_file'):
+			if invoice_file_data:
+				invoice_filename = "invoice-upload-"+str(int(time.time()))+".xlsx"
+				invoice_file_data.save(import_dirname+invoice_filename)
+				msg,status = consolidateRecord.main(import_dirname+invoice_filename)
+				if status:
+					flash('Invoice added to db.')
+				else:
+					flash("Error: %s" % str(msg))
 			else:
-				flash("Error: %s" % str(msg))
-		else:
-			flash('Error: No selected file.')
+				flash('Error: No selected file.')
+		return redirect(url_for("upload_invoice"))
 	return render_template("import.html")
 
 def randomword(length):
