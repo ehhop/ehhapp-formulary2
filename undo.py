147d60714d79f9f26001959b550153908e62cf67
dchary <daniel.charytonowicz@icahn.mssm.edu>
2018-02-11 18:18:41 -0500

    Undo Function & DrugMatch Algorithm
    Undo function has been added, takes an invoice checksum and uses it to clear databsase of any record from that invoice, updates persistantMedication ito clear a record if no histoy entries are present.
    
    DrugMatch Algorithm updated to now source from a secondary API utilizing manufacturer catalog ontologies to attempt to recover a name if rxNorm API can't identify by name.

--- undo.py
+++ undo.py
@@ -1,3 +1,43 @@
 '''
-Removes all medicationHistory database references with the given 
+Removes all medicationHistory database references with the given
+
+- 1. Delete the Invoice from the invoice table
+- 2. Go to PersistantMedicaion and check for what is blank
+
+        db_session.delete(inv).commit()
+
 '''
+import database
+def undo(invoiceChecksum):
+    #check if we  have anything
+    query = database.Invoice.query.filter(database.Invoice.checksum.is_(invoiceChecksum)).all()
+    #quit if there is no query
+    if not query:
+        print "no result!"
+        return
+    #get the proper invoiceId from invocies to delete the invoice record
+    invoiceId =  query[0].id
+
+    #delete invoice
+    database.Invoice.query.filter(database.Invoice.checksum.is_(invoiceChecksum)).delete()
+
+    #delete the invoicerecord if it matches the invoiceID number from the invoice
+    database.InvoiceRecord.query.filter(database.InvoiceRecord.invoice_id.is_(invoiceId)).delete()
+
+    #delete medicationHistory using the invocieChecksum
+    database.MedicationHistory.query.filter(database.MedicationHistory.origin.is_(invoiceChecksum)).delete()
+
+    #commit deletions
+    database.ver_db_session.commit()
+
+    #delete persistant medication if the ID shown in persistantmedication is not found anywhere in medication history
+    persistMeds =  database.PersistentMedication.query.all()
+    for persistantMed in persistMeds:
+        medId = persistantMed.id
+        if (len(database.MedicationHistory.query.filter(database.MedicationHistory.medication_id.is_(medId)).all()) == 0):
+            database.PersistentMedication.query.filter(database.PersistentMedication.id.is_(medId)).delete()
+
+    #commit deletions again
+    database.ver_db_session.commit()
+if __name__ == '__main__':
+    undo("2ca8ba9b69e35c0d575cd814a67c9da2e0e06b48")
