1cb6edd11fb583dc5d07effef1a4a4dd3e84facd
nosarcasm <raneff@gmail.com>
2018-05-02 19:30:17 -0400

    fixed consolidaterecord

--- consolidateRecord.py
+++ consolidateRecord.py
@@ -16,7 +16,7 @@ import pandas as pd
 import datetime
 import drugmatch
 
-actualcolumns= ["Exp Code",
+actualcolumns= ["Exp Code",  #these are the column names in our Invoice file headers
                 "Supply Loc",
                 "Item No",
                 "Item Description",
@@ -34,7 +34,7 @@ actualcolumns= ["Exp Code",
                 "Extended Price",
                 "Cost Center No"]
 
-invoice_columns = ['exp_code',
+invoice_columns = ['exp_code',  #these are columns that correspond to our invoice in the DB
                  'supply_loc',
                  'item_no',
                  'item_description',
@@ -56,6 +56,7 @@ invoice_columns = ['exp_code',
 ## from https://www.pythoncentral.io/hashing-files-with-python/
 import hashlib
 def hash_file(filename):
+    '''creates hashes from files so that we can uniquely identify uploads'''
     BLOCKSIZE = 65536
     hasher = hashlib.sha1()
     with open(filename, 'rb') as afile:
@@ -66,32 +67,35 @@ def hash_file(filename):
     return hasher.hexdigest()
 
 def saveinvoicetodb(file):
+    '''takes a invoice file, checks the hash, adds invoice records, and saves to persistentmedications'''
     ds = pd.read_excel(file,usecols=range(0,17))
     ds = ds.dropna(axis=0,thresh=len(ds.columns)-3)
     ds = ds[[col for col in actualcolumns if col in ds.columns]]
     invoiceobjs = []
     hashMe = hash_file(file)
-    invoice_db, is_imported = database.get_or_create(database.Invoice,checksum=hashMe)
-    if is_imported:
+    invoice_db, is_imported = database.get_or_create(database.Invoice,checksum=hashMe) #check if invoice already was uploaded
+    if is_imported: #if so, reject invoice and return here
         return False,hashMe
+    # if not, populate invoice object
     invoice_db.filename=file
     invoice_db.date_added = datetime.datetime.now()
-    for ix,row in ds.iterrows():
-        newobj = database.InvoiceRecord(invoice_id=invoice_db.id)
-        for df_col in ds.columns:
-            col = invoice_columns[actualcolumns.index(df_col)]
-            setattr(newobj,col,str(row[df_col]))
+    for ix,row in ds.iterrows(): #populate invoicerecords with data from invoice's rows
+        ## FOR ROW IN THE INVOICE ##
+        newobj = database.InvoiceRecord(invoice_id=invoice_db.id) #create a new DB InvoiceRecord obj
+        for df_col in ds.columns: #for column in invoice's  row
+            col = invoice_columns[actualcolumns.index(df_col)] #convert the column name to the one we want for the DB
+            setattr(newobj,col,str(row[df_col])) #set the property of InvoiceRecord to the column that correpsonds to it
             #print(col,getattr(newobj,col))
         #break
-        invoice_db.records.append(newobj)
+        invoice_db.records.append(newobj) #we append all of the invoicerecords to the Invoice DB object for later
 
-    data = dict()
+    data = dict() ## ???
 
-    originInvoiceHash = hashMe 
-    for ix,row in ds.iterrows():
+    originInvoiceHash = hashMe
+    for ix,row in ds.iterrows(): #iterate through each row in invoice XLS (WHY NO INVOICERECORD HERE WTF)
         pricetable_id = int(row["Item No"])           #Medication ID
 
-        if numpy.isnan(pricetable_id):
+        if numpy.isnan(pricetable_id): #if no pricetable id, it's useless
             continue
 
         medication_name = row["Item Description"]         #Medicaction Name
@@ -99,7 +103,7 @@ def saveinvoicetodb(file):
         qty = int(row["Issue Qty"])             #Quantity of Medication issued
         price = float(row["Extended Price"])/qty       #Medication price
         date_issued = row["Requisition Date"]
-        category = row["Comdty Name "] if "Comdty Name " in row else ""
+        category = row["Comdty Name "] if "Comdty Name " in row else "" #this is a bug in older invoices that we handle (e.g. circa 2014-2015)
         date_issued = date_issued.to_pydatetime()
 
         #Insantiate a new Medication record
@@ -107,42 +111,33 @@ def saveinvoicetodb(file):
             name = medication_name,\
             category=category,\
             transactions = [MedicationRecord.transaction(date = date_issued,price = price,qty = qty,originInvoiceHash = originInvoiceHash)])
-        #Check if the record is in database, if not add it
+        #Check if the record has already been created for that med, else make a new one
         if record in data:
             #since the record is in the database, update transactions
             old_record = data[record]
             old_record.transactions += record.transactions
         else:
-            #Add record to database, only here to query to save time
-            dosage, admin, common_name, cui = drugmatch.rxGetDrugProperties(medication_name,mfgID)
-            record.dosage = dosage
-            record.admin = admin
-            record.common_name = common_name
-            record.cui = cui
-            data[record] = record
+            #Add record to dict of MedicationRecords
+            data[record] = record #hold temporary MedicationRecords
 
     #print("HashOrigin Is:"+str(originInvoiceHash))
     persistentmeds = []
     for key, value in data.items():
         #print value.transactions
-        persistentmeds.append(database.save_persistent_record(value, commit=False)) #cross your fingers!
-            #print(value)
+        persistent_med = database.save_persistent_record(value, commit=False)
+        if persistent_med.cui == None:
+            dosage, admin, common_name, cui = drugmatch.rxGetDrugProperties(persistent_med.name,mfgID)
+            persistent_med.dosage = dosage
+            persistent_med.admin = admin
+            persistent_med.common_name = common_name
+            persistent_med.cui = cui
+        persistentmeds.append(persistent_med)
+
+    #add everything to sqlalchemy session and commit it
     database.ver_db_session.add(invoice_db)
     database.ver_db_session.add_all(persistentmeds)
     database.ver_db_session.commit() #cross your fingers!
     return True, hashMe
-    #Generate PersistntMedication Record for each item, and add to database
-
-def save_hash_for_invoice(invoice_id):
-    try:
-        invoice_db = database.Invoice.query.get(invoice_id)
-        hashMe = hash_file(invoice_db.filename)
-        invoice_db.checksum = hashMe
-        database.ver_db_session.add(invoice_db)
-        database.ver_db_session.commit()
-    except BaseException as msg:
-        return "An error occurred: %s" % msg
-    return None
 
 def main(filename):
     try:
@@ -157,5 +152,4 @@ def main(filename):
 
 if __name__ == '__main__':
         saveinvoicetodb("invoice.xls")
-        readrecord("invoice.xls")
         print("Done reading all records")
